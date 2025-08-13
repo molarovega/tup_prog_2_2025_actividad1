05f2d51805a19a5d1f0d26bf4228981c37231ba7
molarovega <molarofrancisco@hotmail.com>
2025-08-13 15:29:24 -0300

    ejercicio 1

---
+++ Guia1/Ejercicio1/Models/Ortoedro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class Ortoedro
+    {
+        public Rectangulo[] Laterales = new Rectangulo[4];
+        public Rectangulo[] Bases = new Rectangulo[2];
+        public Ortoedro(double anchoBase, double ladoComun, double largoLateral)
+        {
+            Bases[0] = new Rectangulo(anchoBase, ladoComun);
+            Bases[1] = new Rectangulo(anchoBase, ladoComun);
+
+            Laterales[0] = new Rectangulo(ladoComun, largoLateral);
+            Laterales[1] = new Rectangulo(ladoComun, largoLateral);
+            Laterales[2] = new Rectangulo(ladoComun, largoLateral);
+            Laterales[3] = new Rectangulo(ladoComun, largoLateral);
+        }
+
+        public double CalcularArea()
+        {
+            double areaBase = Bases[0].CalcularArea();
+            double areaLateral = Laterales[0].CalcularArea();
+            return areaBase * 2 + areaLateral * 4;
+        }
+
+        public double CalcularVolumen()
+        {
+            return Bases[0].CalcularArea() * Laterales[0].Largo;
+        }
+    }
+}
