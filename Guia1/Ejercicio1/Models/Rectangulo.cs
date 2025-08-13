1cce11ca84374d4aed8d41548a9468b05f78ce09
molarovega <molarofrancisco@hotmail.com>
2025-08-13 15:34:51 -0300

    ejercicio 1

--- Guia1/Ejercicio1/Models/Rectangulo.cs
+++ Guia1/Ejercicio1/Models/Rectangulo.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ejercicio1.Models
 {
-    internal class Rectangulo
+    public class Rectangulo
     {
+        public double Ancho { get; set; }
+        public double Largo { get; set; }
+
+        public Rectangulo(double ancho, double largo)
+        {
+            Ancho = ancho;
+            Largo = largo;
+        }
+        //
+        public double CalcularArea()
+        {
+            return Largo * Ancho;
+        }
+        public string Describir()
+        {
+            var culture = CultureInfo.InvariantCulture;
+
+            return $@"
+          {{ 
+             ""Tipo"": ""Rectangulo"", 
+             ""Largo"": {Largo.ToString("f2", culture)}, 
+             ""Ancho"": {Ancho.ToString("f2", culture)}, 
+             ""Area"": {CalcularArea().ToString("f2", culture)}
+          }}";
+
+        }
     }
 }
