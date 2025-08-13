9d2758d96360880c09083bec7fcc70964cd2a639
molarovega <molarofrancisco@hotmail.com>
2025-08-13 15:49:22 -0300

    ejercicio1

--- Guia1/Ejercicio1/Form1.Designer.cs
+++ Guia1/Ejercicio1/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbResultados = new System.Windows.Forms.TextBox();
+            this.btnPrueba = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbResultados
+            // 
+            this.tbResultados.Location = new System.Drawing.Point(22, 25);
+            this.tbResultados.Multiline = true;
+            this.tbResultados.Name = "tbResultados";
+            this.tbResultados.Size = new System.Drawing.Size(420, 391);
+            this.tbResultados.TabIndex = 0;
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(569, 170);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(153, 66);
+            this.btnPrueba.TabIndex = 1;
+            this.btnPrueba.Text = "Prueba";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrueba);
+            this.Controls.Add(this.tbResultados);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbResultados;
+        private System.Windows.Forms.Button btnPrueba;
     }
 }
 
